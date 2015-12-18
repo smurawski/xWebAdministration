a3fd323882cb8b26905cc756e947f5e508cec92c
Steven Murawski <steven.murawski@gmail.com>
2015-12-18 16:41:43 -0600

    First cut with test-kitchen

--- Examples/Sample_xWebsite_NewWebsite.ps1
+++ Examples/Sample_xWebsite_NewWebsite.ps1
@@ -1,31 +1,23 @@
 configuration Sample_xWebsite_NewWebsite
 {
-    param
-    (
-        # Target nodes to apply the configuration
-        [string[]]$NodeName = 'localhost',
+    # This configuration expects that $ConfigurationData will have one or more
+    # nodes with three keys:
 
-        # Name of the website to create
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$WebSiteName,
-
-        # Source Path for Website content
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$SourcePath,
-
-        # Destination path for Website content
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$DestinationPath
-    )
+    # SourceUrl - which is the zip file with the BakeryWebsite demo site
+    # DestinationPath - which is where the IIS site files will be saved
+    # WebSiteName - Name of the website to host the BakeryWebsite
 
     # Import the module that defines custom resources
     Import-DscResource -Module xWebAdministration, PSDesiredStateConfiguration
 
-    Node $NodeName
+    Node $AllNodes.NodeName
     {
+        $MandatoryNodeData = 'SourceUrl', 'DestinationPath', 'WebSiteName'
+        if ($MandatoryNodeData.where({-not $Node.ContainsKey($_)}))
+        {
+            throw 'Nodes in $ConfigurationData must contain SourceUrl, DestinationPath, and WebSiteName properties.'
+        }
+
         # Install the IIS role
         WindowsFeature IIS
         {
@@ -38,38 +30,61 @@ configuration Sample_xWebsite_NewWebsite
         {
             Ensure          = 'Present'
             Name            = 'Web-Asp-Net45'
+            DependsOn       = '[WindowsFeature]IIS'
         }
 
         # Stop the default website
-        xWebsite DefaultSite 
+        xWebsite DefaultSite
         {
             Ensure          = 'Present'
             Name            = 'Default Web Site'
             State           = 'Stopped'
             PhysicalPath    = 'C:\inetpub\wwwroot'
-            DependsOn       = '[WindowsFeature]IIS'
+            DependsOn       = '[WindowsFeature]AspNet45'
         }
 
-        # Copy the website content
+        # Grab a zip file of the demo
+        script DownloadSample
+        {
+           GetScript = "@{}"
+           SetScript = "irm $($Node.SourceUrl) -outfile c:\bakery.zip"
+           TestScript = "test-path c:\bakery.zip"
+        }
+
+        # Extract the zipfile of demo files.
+        Archive DemoFiles
+        {
+            Destination = 'c:\demo'
+            Path = 'c:\bakery.zip'
+            DependsOn = '[Script]DownloadSample'
+        }
+
+        # Setup the directory to host the website content.
+        File SiteDirectory
+        {
+            DestinationPath = (Split-Path $Node.DestinationPath)
+            Type = 'Directory'
+        }
+
+        # Copy the website content from the demo download.
         File WebContent
         {
-            Ensure          = 'Present'
-            SourcePath      = $SourcePath
-            DestinationPath = $DestinationPath
-            Recurse         = $true
-            Type            = 'Directory'
-            DependsOn       = '[WindowsFeature]AspNet45'
-        }       
+            DestinationPath = $Node.DestinationPath
+            SourcePath = 'c:\demo\Demo_WindowServer2012R2-Preview\PreReq\BakeryWebsite\'
+            Recurse = $true
+            Force = $true
+            MatchSource = $true
+            DependsOn = @('[Archive]DemoFiles', '[File]SiteDirectory')
+        }
 
         # Create the new Website
         xWebsite NewWebsite
         {
             Ensure          = 'Present'
-            Name            = $WebSiteName
+            Name            = $Node.WebSiteName
             State           = 'Started'
-            PhysicalPath    = $DestinationPath
-            DependsOn       = '[File]WebContent'
+            PhysicalPath    = $Node.DestinationPath -replace '/', '\'
+            DependsOn       = @('[WindowsFeature]AspNet45', '[File]WebContent')
         }
     }
 }
-
